452feeca1e96bc1be88b8ac4b4738411150331da
Eric Kivel <ericvkivel1@gmail.com>
2022-01-01 22:54:33 -0300

    Starting Chapter 4

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,18 @@
+describe("Create Category", () => {
+
+    it("Should be able to create a category", () => {
+        const soma = 2 + 2;
+
+        const resultado = 4;
+
+        expect(soma).toBe(resultado);
+    });
+
+    it("Should be able to create a category", () => {
+        const soma = 2 + 2;
+
+        const resultado = 5;
+
+        expect(soma).not.toBe(resultado);
+    });
+});
