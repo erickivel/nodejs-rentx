1f06eae5498ab36a8886fcd2bc4f67c8f091710c
Eric Kivel <ericvkivel1@gmail.com>
2022-01-17 17:53:52 -0300

    Sentry Configuration

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -4,6 +4,8 @@ import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
 import swaggerUi from 'swagger-ui-express';
 import cors from "cors";
+import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 
 import createConnection from "@shared/infra/typeorm";
 import swaggerFile from '../../../swagger.json';
@@ -18,6 +20,18 @@ const app = express();
 
 app.use(rateLimiter)
 
+Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+});
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(express.json());
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
@@ -26,9 +40,9 @@ app.use("/avatar", express.static(`${upload.tmpFolder}/avatar`));
 app.use("/cars", express.static(`${upload.tmpFolder}/cars`));
 
 app.use(cors());
-
-
 app.use(router);
+
+app.use(Sentry.Handlers.errorHandler());
 
 app.use(
     (err: Error, request: Request, response: Response, next: NextFunction) => {
